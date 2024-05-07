6567d0e37fba99ce91369e54283f1f1315b362b6
PedroLucasCruz <pedrolucas11@live.com>
2024-05-06 22:03:17 -0300

    1 - Estrutura inicial para testes. 2 - Adicionado estruturas iniciais para validar entidades.

---
+++ Fiap.CleanArchitecture.Tests/Entitty/TarefaTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.CleanArchitecture.Tests.Entitty
+{
+    public class TarefaTests
+    {
+        [Fact]
+        public void Tarefa_Validar_Titulo_Tamanho()
+        {
+
+        }
+    }
+}
---
+++ Fiap.CleanArchitecture.Tests/Entitty/UsuarioTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.CleanArchitecture.Tests.Entitty
+{
+    public class UsuarioTests
+    {
+        [Fact]
+        public void Usuario_Validar_Nome_Tamanho()
+        {
+
+        }
+    }
+}
